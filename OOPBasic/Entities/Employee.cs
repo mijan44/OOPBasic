0e146a3d37556700d0dbb78d607033230a6c2bbf
mijanur <mijannao@gmail.com>
2024-05-14 17:22:30 +0800

    Add project files.

---
+++ OOPBasic/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasic.Entities
+{
+    public class Employee : IEmployee
+    {
+
+        public void AddEmployee(string EmployeeName)
+        {
+            Console.WriteLine($"Employee Name : {EmployeeName}");
+        }
+           
+    }
+}
---
+++ OOPBasic/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OOPBasic.Entities;
+using System.Runtime.InteropServices;
+using System.Security.Authentication.ExtendedProtection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IEmployee, Employee>()
+            .BuildServiceProvider();
+
+        var employeeService = serviceProvider.GetService<IEmployee>();
+        employeeService.AddEmployee("mijan");
+        Console.WriteLine();
+    }
+}
